3a2d2f96bf348db97982b5c769027fed8c45452b
Yochannah Yehudi <yochannah@gmail.com>
2015-01-01 21:30:51 +0000

    added default pre-search behaviour

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ var gitAppControllers = angular.module('gitAppControllers', []);
 
 gitAppControllers.controller('IssuesListController', ['$scope','Issue', function($scope, Issue) {
 
-	$scope.issues = {}
+	$scope.issues = [];
 	
 	$scope.search = function() {
 	    $scope.issues = Issue.query($scope.filters, function (success) {
