a25b2efb43bbafa0cbfb3f919411c828e8d6f8c1
Yochannah Yehudi <yochannah@gmail.com>
2015-01-01 23:02:02 +0000

    added loader

--- js/controllers.js
+++ js/controllers.js
@@ -5,18 +5,23 @@ gitAppControllers.controller('IssuesListController', ['$scope','Issue', function
 	$scope.issues = [];
 	
 	$scope.search = function() {
-	    $scope.issues = Issue.query($scope.filters, function (success) {
-	    }, function (error) {
-	        $scope.error = error;
+	    $scope.loading = true;
+	    $scope.errorResponse = false;
+	    $scope.issues = Issue.query($scope.filters, 
+	        function (success) {
+	            $scope.loading = false;
+	        }, 
+	        function (error) {
+	            $scope.loading = false;
+    	        $scope.error = error;
+                //we'll add advanced error handling later, for now this should do. Ideally, let them know if the user exists even if the repo doesn't!
+                if($scope.error.status === "404") {
+                    $scope.errorResponse = "It looks like the user or repo you were looking for couldn't be found. Please check if you've spelled everything correctly, and make sure it's not a private repo!";
+                } else {
+                    $scope.errorResponse = "Oops. Looks like there has been an error. We've reported it to the computer elves and we're doing our best to fix it right now."
+                }
 	        
-            //we'll add advanced error handling later, for now this should do. Ideally, let them know if the user exists even if the repo doesn't!
-	        if($scope.error.status === "404") {
-	            $scope.errorResponse = "It looks like the user or repo you were looking for couldn't be found. Please check if you've spelled everything correctly, and make sure it's not a private repo!";
-	        } else {
-	            $scope.errorResponse = "Oops. Looks like there has been an error. We've reported it to the computer elves and we're doing our best to fix it right now."
-	        }
-	        
-	    });
+    	    });
 	};
 
 }]);
