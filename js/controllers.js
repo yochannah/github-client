96e0c7ff630cf4f658f8ea2eda86a200a1cbea4b
Yochannah Yehudi <yochannah@gmail.com>
2015-01-02 16:30:04 +0000

    fixed 404 error return - previously hadn't ever been returning

--- js/controllers.js
+++ js/controllers.js
@@ -15,8 +15,8 @@ gitAppControllers.controller('IssuesListController', ['$scope','Issue', function
 	            $scope.loading = false;
     	        $scope.error = error;
                 //we'll add advanced error handling later, for now this should do. Ideally, let them know if the user exists even if the repo doesn't!
-                if($scope.error.status === "404") {
-                    $scope.errorResponse = "It looks like the user or repo you were looking for couldn't be found. Please check if you've spelled everything correctly, and make sure it's not a private repo!";
+                if($scope.error.status == "404") {
+                    $scope.errorResponse = "404: It looks like the user or repo you were looking for couldn't be found. Please check if you've spelled everything correctly, and make sure it's not a private repo!";
                 } else {
                     $scope.errorResponse = "Oops. Looks like there has been an error. We've reported it to the computer elves and we're doing our best to fix it right now."
                 }
