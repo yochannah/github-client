80a6029ae72fb37e383de00d26c2fa49f8bf8cb5
Yochannah Yehudi <yochannah@gmail.com>
2015-01-03 13:05:32 +0000

    refactored controllers as they weren't looking too DRY

--- js/controllers.js
+++ js/controllers.js
@@ -6,31 +6,35 @@ gitAppControllers.controller('IssuesListController', ['$scope','Issue', 'User', 
 	$scope.repos = [];
 	
 	$scope.search = function() {
-	    $scope.loading = true;
-	    
-	    $scope.errorResponse = false;
-	    
-	    $scope.issues = Issue.query($scope.filters, 
-	        function (success) {
-	            $scope.loading = false;
-	        }, 
-	        function (error) {
-	            $scope.loading = false;
-    	        $scope.error = error;
-                //we'll add advanced error handling later, for now this should do. Ideally, let them know if the user exists even if the repo doesn't!
-                if($scope.error.status == "404") {
-                    $scope.errorResponse = "404: It looks like the user or repo you were looking for couldn't be found. Please check if you've spelled everything correctly, and make sure it's not a private repo!";
-                } else {
-                    $scope.errorResponse = "Oops. Looks like there has been an error. We've reported it to the computer elves and we're doing our best to fix it right now."
-                }
-	        
-    	    });    
-    	};
+        $scope.searchFor(Issue,'issues');
+    };
     	
-	    $scope.searchUsers = function() {
-	        var repos = User.query($scope.filters, function () {
-	            $scope.repos = repos;
-	        });	            	    
+    $scope.searchUsers = function() {
+        $scope.searchFor(User,'repos');
+    };
+
+    $scope.searchFor = function(thingToFind, storeAs) {
+        $scope.loading = true;
+        $scope.errorResponse = false;
+
+        $scope[storeAs] = thingToFind.query(
+        $scope.filters, 
+        function (success) {
+            console.log(success, storeAs);
+            $scope.loading = false;
+        }, 
+        function (error) {
+            $scope.loading = false;
+            $scope.error(error);
+        });
+    };
+
+    $scope.error = function(error) {
+    	    if(error.status == "404") {
+                $scope.errorResponse = "404: It looks like the user you were looking for couldn't be found. Please check if you've spelled everything correctly!";
+            } else {
+                $scope.errorResponse = "Oops. Looks like there has been an error. We've reported it to the computer elves and we're doing our best to fix it right now."
+            }
     	};
 	
 }]);
